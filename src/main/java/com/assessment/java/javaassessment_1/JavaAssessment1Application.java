7add9d5d02751fb60dd3f6f0061085ef76aa480f
Yarragudipadu <naveen-kesav.yarragudipadu@capgemini.com>
2024-02-07 16:28:07 +0530

    first commit

---
+++ src/main/java/com/assessment/java/javaassessment_1/JavaAssessment1Application.java
@@ -0,0 +1,13 @@
+package com.assessment.java.javaassessment_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssessment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAssessment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/java/javaassessment_1/JavaAssessment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.java.javaassessment_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssessment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
